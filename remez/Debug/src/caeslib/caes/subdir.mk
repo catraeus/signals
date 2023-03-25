693f54699679e99df18ff39ae0e2e0213fda4b85
Duncan Gray <duncanbranch@gmail.com>
2023-03-24 21:16:47 -0500

    remez project added

---
+++ remez/Debug/src/caeslib/caes/subdir.mk
@@ -0,0 +1,37 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CPP_SRCS += \
+../src/caeslib/caes/CaesHpNum.cpp \
+../src/caeslib/caes/CaesMath.cpp \
+../src/caeslib/caes/CaesRemez.cpp 
+
+CPP_DEPS += \
+./src/caeslib/caes/CaesHpNum.d \
+./src/caeslib/caes/CaesMath.d \
+./src/caeslib/caes/CaesRemez.d 
+
+OBJS += \
+./src/caeslib/caes/CaesHpNum.o \
+./src/caeslib/caes/CaesMath.o \
+./src/caeslib/caes/CaesRemez.o 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+src/caeslib/caes/%.o: ../src/caeslib/caes/%.cpp src/caeslib/caes/subdir.mk
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C++ Compiler'
+	g++ -I/way/dev/SW/caeslib -I/usr/include/sigc++-2.0 -I/usr/lib/arch/sigc++-2.0/include -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
+clean: clean-src-2f-caeslib-2f-caes
+
+clean-src-2f-caeslib-2f-caes:
+	-$(RM) ./src/caeslib/caes/CaesHpNum.d ./src/caeslib/caes/CaesHpNum.o ./src/caeslib/caes/CaesMath.d ./src/caeslib/caes/CaesMath.o ./src/caeslib/caes/CaesRemez.d ./src/caeslib/caes/CaesRemez.o
+
+.PHONY: clean-src-2f-caeslib-2f-caes
+
---
+++ remez/src/_main.cpp
@@ -0,0 +1,144 @@
+
+// $Id: _main.cpp 747 2023-03-05 21:15:19Z duncang $
+
+//============================================================================
+// Name        : _main.cpp
+// Author      : Catraeus & Duncan Gray
+// Version     :
+// Copyright   : Copyright © 2014 - 2019 Catraeus & Duncan Gray
+// Description : Wrapper to exercise and expose the Remez class.
+//============================================================================
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <caes/CaesRemez.hpp>
+#include <caes/CaesMath.hpp>
+
+int main(int argc, char *argv[]) {
+  ldouble   *weights;
+  ldouble   *desired;
+  ldouble   *bands;
+  ldouble   *h;
+  double    *j;
+  llong     i;
+  llong     FIRlen;
+  llong     bandCount;
+  ldouble   normFreq;  // DANGER  If you make this DC (0.0) and the length is even then it'll blow up.
+  bool      doNorm;
+
+  Remez     R;
+
+
+
+  //=========================
+  // Set up the Remez
+  bands      = new ldouble[   16]; // Just size it to the biggest
+  weights    = new ldouble[    8]; // Just size it to the biggest
+  desired    = new ldouble[    8]; // Just size it to the biggest
+  h          = new ldouble[32768]; // Just size it to the biggest
+
+#if 0
+
+  FIRlen     = 2047;
+  bandCount  =    2;
+
+  desired[0] =    1.0;
+  desired[1] =    0.0;
+  desired[2] =    0.0;
+  desired[3] =    0.0;
+  desired[4] =    0.0;
+
+  weights[0] =    1.0;
+  weights[1] = 2150.0;
+  weights[2] =    1.0;
+  weights[3] =    1.0;
+  weights[4] =    1.0;
+
+  bands[0]   =                 0.0;
+  bands[1]   =    ((ldouble)  431.0) / ((ldouble)48000.0);
+  bands[2]   =    ((ldouble)  500.0) / ((ldouble)48000.0);
+  bands[3]   =    ((ldouble)24000.0) / ((ldouble)48000.0);
+  bands[4]   =    0.50000;
+  bands[5]   =    0.50000;
+  bands[6]   =    0.50000;
+  bands[7]   =    0.50000;
+  bands[8]   =    0.50000;
+  bands[9]   =    0.50000;
+
+  normFreq   =    0.00000;
+  doNorm     =    true;
+
+#else
+  FIRlen     =  1922;
+  bandCount  =     3;
+
+  desired[0] =     0.0;
+  desired[1] =     1.0;
+  desired[2] =     0.0;
+  desired[3] =     0.0;
+  desired[4] =     0.0;
+
+  weights[0] =   703.0;
+  weights[1] =     1.0;
+  weights[2] =   703.0;
+  weights[3] =     1.0;
+  weights[4] =     1.0;
+
+  bands[0]   =                  0.0;
+  bands[1]   =    ((ldouble)  500.0) / ((ldouble)48000.0);
+  bands[2]   =    ((ldouble)  573.0) / ((ldouble)48000.0);
+  bands[3]   =    ((ldouble) 1427.0) / ((ldouble)48000.0);
+  bands[4]   =    ((ldouble) 1500.0) / ((ldouble)48000.0);
+  bands[5]   =    ((ldouble)24000.0) / ((ldouble)48000.0);
+  bands[6]   =    0.50000;
+  bands[7]   =    0.50000;
+  bands[8]   =    0.50000;
+  bands[9]   =    0.50000;
+
+  normFreq   =    (bands[2] + bands[3]) / 2.00000;
+  doNorm     =    true;
+
+#endif
+
+  fprintf(stdout, "ldouble: %ld     ldouble: %ld\n", sizeof(ldouble), sizeof(ldouble)); fflush(stdout);
+  R.SetTypeBandPass();
+  R.SetN(FIRlen);
+  R.SetNumB(bandCount);
+  R.SetAry(h);
+  for(llong i = 0; i < bandCount; i++) {
+    R.SetEdge(i * 2,     bands[i * 2    ]);
+    R.SetEdge(i * 2 + 1, bands[i * 2 + 1]);
+    R.SetG(i, desired[i]);
+    R.SetW(i, weights[i]);
+  }
+
+  R.Calculate();
+  if(doNorm)
+    R.NormSF(normFreq);
+  //ldouble corrector = 1.0 / h[(ullong)R.GetN() / 2];
+  //for (i=0; i<R.GetN(); i++) h[i] *= corrector;
+  for (i=0; i<R.GetN(); i++) fprintf(stdout, "%37.30Le\n", h[i]);
+
+  ldouble peak;
+  peak = -1.0;
+  for(i=0; i<FIRlen; i++)
+    if(peak < dabs(h[i]))
+      peak = dabs(h[i]);
+  for(i=0; i<FIRlen; i++)
+    h[i] /= peak;
+
+  j = new double[FIRlen];
+  for(llong k=0; k<FIRlen; k++)
+    j[k] = (double)(h[k]);
+  FILE *blobFile = fopen("blob.raw", "wb");
+  fwrite((void *)(j), sizeof(double), FIRlen, blobFile);
+  fclose(blobFile);
+  delete(bands);
+  delete(weights);
+  delete(desired);
+  delete(h);
+  delete(j);
+
+  fprintf(stderr, "\n"); fflush(stderr);
+  return 0;
+  }
