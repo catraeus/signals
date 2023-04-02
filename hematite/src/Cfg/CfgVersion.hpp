d8fcd65676544b89f0876d2ba398bd779a2de736
Duncan Gray <duncanbranch@gmail.com>
2023-04-02 09:40:42 -0500

    testing globalized comitit

--- hematite/src/Cfg/CfgVersion.hpp
+++ hematite/src/Cfg/CfgVersion.hpp
@@ -11,7 +11,7 @@
 //
 //=================================================================================================
 
-#define GIT_MARK     763
+#define GIT_MARK     767
 #define VER_MAIN      1
 #define VER_POINT     0
 #define APP_NAME     "hematite"
