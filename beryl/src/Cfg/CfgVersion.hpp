4a54818f52734ae175c9912a8bf93055196d5c38
Duncan Gray <duncanbranch@gmail.com>
2023-08-30 18:24:11 -0500

    No clue

--- beryl/src/Cfg/CfgVersion.hpp
+++ beryl/src/Cfg/CfgVersion.hpp
@@ -15,7 +15,7 @@
 
 #ifndef __CFG_VERSION_HPP_
 #define __CFG_VERSION_HPP_
-#define GIT_MARK     758
+#define GIT_MARK     759
 #define VER_MAIN       0
 #define VER_POINT      1
 #define APP_NAME     "beryl"
